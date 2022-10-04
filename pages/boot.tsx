5e95acc71b4c5f72df5dfcec386f7aaf2428036d
Felipe Paz <haohmaru@localhost.localdomain>
2022-10-04 20:25:42 -0300

    feat: import custom google font

--- pages/boot.tsx
+++ pages/boot.tsx
@@ -1,5 +1,48 @@
 import Link from 'next/link';
 import React from 'react';
+import styled from 'styled-components';
+
+const MainContainer = styled('main')`
+  display: flex;
+  background-color: #000;
+  color: #fff;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  font-family: 'Roboto Slab', serif;
+`;
+
+const BodyContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  background-color: red;
+  width: 60%;
+  height: 70%;
+
+`;
+
+const UlContainer = styled('ul')`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const LiOption = styled('li')`
+  list-style: none;
+  margin: 0;
+  padding: 5px 0;
+  margin-bottom: 10px;
+  cursor: pointer;
+  :hover {
+    background-color: #fff;
+    color: #000;
+  }
+`;
 
 const BootPage = () => {
 
@@ -17,22 +60,25 @@ const BootPage = () => {
   const boot = (os: string): void => localStorage.setItem('booted', os);
 
   return (
-    <div>
-      Boot Page
-      <ul>
-        {
-          operationalSystems.map((o, i) => {
-            return <li key={ i }>
-              <Link href="/" >
-                <span onClick={ () => boot(o.value) }>
-                  { o.name }
-                </span>
-              </Link>
-            </li>;
-          })
-        }
-      </ul>
-    </div>
+    <MainContainer>
+      <BodyContainer>
+
+        Boot Page
+        <UlContainer>
+          {
+            operationalSystems.map((o, i) => {
+              return <LiOption key={ i }>
+                <Link href="/" >
+                  <span onClick={ () => boot(o.value) }>
+                    { o.name }
+                  </span>
+                </Link>
+              </LiOption>;
+            })
+          }
+        </UlContainer>
+      </BodyContainer>
+    </MainContainer>
   );
 };
 
